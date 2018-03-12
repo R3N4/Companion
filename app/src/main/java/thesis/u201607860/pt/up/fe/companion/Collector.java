eea95b650a88bb915e8402416159172424f017c5
R3N45 <renamotas@hotmail.com>
2018-03-12 17:37:57 +0000

    Sensors Added
    Added Orientation & Geomagnetic Rotation Sensors;
    Doesn't seem like the events are triggering.

--- app/src/main/java/thesis/u201607860/pt/up/fe/companion/Collector.java
+++ app/src/main/java/thesis/u201607860/pt/up/fe/companion/Collector.java
@@ -18,13 +18,30 @@ public class Collector extends Service implements SensorEventListener {
     private Sensor _accelerometer;
     private Sensor _gyroscope;
     private Sensor _orientation;
+    private Sensor _magnetic;
     private String _fileAccelerometer = "accelData";
     private String _fileGyroscope = "gyroData";
     private String _fileOrientation = "orienData";
+    private String _fileMagRot = "magneticData";
     private FileOutputStream _fileStreamAccel;
     private FileOutputStream _fileStreamGyro;
     private PrintWriter accelWriter;
     private PrintWriter gyroWriter;
+    private PrintWriter orienWriter;
+    private PrintWriter magneticWriter;
+
+    private PrintWriter InitiateWriter(String fileName, Long timestamp){
+        try {
+            return new PrintWriter(getApplicationContext().getExternalFilesDir(null)+"/"+fileName+".session"+timestamp+".csv", "UTF-8");
+        }catch (Exception e){
+            System.out.println(e.toString());
+        }
+        return null;
+    }
+
+    private void WriteHeader(PrintWriter writer, String[] data){
+        writer.println(data[0]+";"+data[1]+";"+data[2]+";"+data[3]+";"); //Timestamp;X;Y;Z;
+    }
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -41,22 +58,22 @@ public class Collector extends Service implements SensorEventListener {
         _sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         _accelerometer = _sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         _gyroscope = _sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-        //_orientation = _sensorManager.getDefaultSensor(Sensor.TYPE_);
+        _orientation = _sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+        _magnetic = _sensorManager.getDefaultSensor(Sensor.TYPE_GEOMAGNETIC_ROTATION_VECTOR);
 
         _sensorManager.registerListener(this, _accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 
-        try{
-            Long time = System.currentTimeMillis();
-            System.out.println();
-            System.out.println(getApplicationContext().getExternalFilesDir(null)+"/"+_fileGyroscope+".session"+time);
-            gyroWriter = new PrintWriter(getApplicationContext().getExternalFilesDir(null)+"/"+_fileGyroscope+".session"+time, "UTF-8");
+        Long time = System.currentTimeMillis();
+        System.out.println(getApplicationContext().getExternalFilesDir(null)+"/"+".session"+time);
 
-            accelWriter = new PrintWriter(getApplicationContext().getExternalFilesDir(null)+"/"+ _fileAccelerometer+".session"+time, "UTF-8");
-            //_fileStreamAccel = new FileOutputStream(getApplicationContext().getExternalFilesDir(null)+"/"+_fileAccelerometer+".session"+time, true);
-            //_fileStreamGyro = new FileOutputStream(getApplicationContext().getExternalFilesDir(null)+"/"+_fileGyroscope+".session"+time, true);
-        }catch (Exception e){
-            System.out.println(e.toString());
-        }
+        accelWriter = InitiateWriter(_fileAccelerometer, time);
+        WriteHeader(accelWriter, new String[]{time.toString(), "x Acceleration", "y Acceleration", "z Acceleration"});
+        gyroWriter = InitiateWriter(_fileGyroscope, time);
+        WriteHeader(gyroWriter, new String[]{time.toString(), "Rotation Around x", "Rotation Around y", "Rotation Around z"});
+        orienWriter = InitiateWriter(_fileOrientation, time);
+        WriteHeader(orienWriter, new String[]{time.toString(), "Azimuth", "Pitch", "Roll"});
+        magneticWriter = InitiateWriter(_fileMagRot, time);
+        WriteHeader(magneticWriter, new String[]{time.toString(), "Rotation Along x", "Rotation Along y", "Rotation Along z"});
 
         MainActivity.ChangeState();
         //MainActivity.Rename();
@@ -67,8 +84,12 @@ public class Collector extends Service implements SensorEventListener {
     @Override
     public void onDestroy(){
         _sensorManager.unregisterListener(this);
+
         accelWriter.close();
         gyroWriter.close();
+        orienWriter.close();
+        magneticWriter.close();
+
         MainActivity.ChangeState();
         //MainActivity.Rename();
     }
@@ -82,35 +103,50 @@ public class Collector extends Service implements SensorEventListener {
     public void onSensorChanged(SensorEvent sensorEvent) {
 
         switch (sensorEvent.sensor.getType()){
+
             case Sensor.TYPE_ACCELEROMETER:
-
-                System.out.println(sensorEvent.timestamp + " :: " + sensorEvent.values[0] + "|" + sensorEvent.values[1] + "|" + sensorEvent.values[2]);
-
+                //System.out.println(sensorEvent.timestamp + " :: " + sensorEvent.values[0] + "|" + sensorEvent.values[1] + "|" + sensorEvent.values[2]);
                 try{
-                    //_fileStreamAccel = openFileOutput(_fileAccelerometer, Context.MODE_APPEND);
-                    String data = sensorEvent.timestamp + " :: " + sensorEvent.values[0] + "|" + sensorEvent.values[1] + "|" + sensorEvent.values[2] + "\n";
-                    //_fileStreamAccel.write(data.getBytes());
-                    //_fileStreamAccel.flush();
-                    //_fileStreamAccel.close();
-                    System.out.println("Error : " + accelWriter.checkError());
+                    String data = sensorEvent.timestamp + ";" + sensorEvent.values[0] + ";" + sensorEvent.values[1] + ";" + sensorEvent.values[2] + ";";
+                    //System.out.println("Error : " + accelWriter.checkError());
                     accelWriter.println(data);
                 } catch (Exception e){
                     e.printStackTrace();
 
                     System.out.println(e.toString());
                 }
-
                 break;
 
             case Sensor.TYPE_GYROSCOPE:
-                //System.out.println("Sensor Activated");
                 try{
-                    //_fileStreamGyro = openFileOutput(_fileGyroscope, Context.MODE_APPEND);
-                    String data = sensorEvent.timestamp + " :: " + sensorEvent.values[0] + "|" + sensorEvent.values[1] + "|" + sensorEvent.values[2] + "\n";
-                    //_fileStreamGyro.write(data.getBytes());
-                    //_fileStreamGyro.close();
-                    System.out.println("Error : " + gyroWriter.checkError());
+                    String data = sensorEvent.timestamp + ";" + sensorEvent.values[0] + ";" + sensorEvent.values[1] + ";" + sensorEvent.values[2] + ";";
+                    System.out.println(sensorEvent.timestamp + " :: " + sensorEvent.values[0] + "|" + sensorEvent.values[1] + "|" + sensorEvent.values[2]);
+                    //System.out.println("Error : " + gyroWriter.checkError());
                     gyroWriter.println(data);
+                } catch (Exception e){
+                    e.printStackTrace();
+
+                    System.out.println(e.toString());
+                }
+                break;
+
+            case Sensor.TYPE_ORIENTATION:
+                try{
+                    String data = sensorEvent.timestamp + ";" + sensorEvent.values[0] + ";" + sensorEvent.values[1] + ";" + sensorEvent.values[2] + ";";
+                    //System.out.println("Error : " + orienWriter.checkError());
+                    orienWriter.println(data);
+                } catch (Exception e){
+                    e.printStackTrace();
+
+                    System.out.println(e.toString());
+                }
+                break;
+
+            case Sensor.TYPE_GEOMAGNETIC_ROTATION_VECTOR:
+                try{
+                    String data = sensorEvent.timestamp + ";" + sensorEvent.values[0] + ";" + sensorEvent.values[1] + ";" + sensorEvent.values[2] + ";";
+                    //System.out.println("Error : " + magneticWriter.checkError());
+                    magneticWriter.println(data);
                 } catch (Exception e){
                     e.printStackTrace();
 
